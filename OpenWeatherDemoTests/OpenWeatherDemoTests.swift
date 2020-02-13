779763c2b3d7e32971e6ee6c5a3ac11cb7d9ec2d
mohamed hussien <mohamed.hussien07@gmail.com>
2020-02-13 17:05:01 +0200

    Initial Commit

---
+++ OpenWeatherDemoTests/OpenWeatherDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  OpenWeatherDemoTests.swift
+//  OpenWeatherDemoTests
+//
+//  Created by mohamed hussein on 2/13/20.
+//  Copyright © 2020 mohamed hussein. All rights reserved.
+//
+
+import XCTest
+@testable import OpenWeatherDemo
+
+class OpenWeatherDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
